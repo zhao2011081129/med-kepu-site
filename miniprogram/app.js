834d4e37d5266bd6bdf6bee8ffc752ec004e4885
yuqian <chaearerahule49@gmail.com>
2026-06-05 21:51:21 +0800

    Point mini program to Render backend

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,7 @@
 App({
   globalData: {
-    // 本地调试可用 http://127.0.0.1:3000；真机/上线请换成 HTTPS 云端地址
-    baseUrl: 'http://127.0.0.1:3000',
+    // 云端后端地址；本地调试可临时改回 http://127.0.0.1:3000
+    baseUrl: 'https://med-kepu-site.onrender.com',
     cookie: '',
     user: null
   }
